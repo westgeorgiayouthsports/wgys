46be5c302c7fa9a2f291e1d80cfe552af930d265
WGYS Admin <westga.youthsports@gmail.com>
2025-12-03 00:39:58 -0500

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   plugins: [react()],
+  // base: mode === 'production' ? '/wgys/' : '/',  base: './',
   base: './',
   build: {
     outDir: 'dist',
