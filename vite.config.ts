a8f38dde6261c197dc4fe32e3fe999e3ca8e7132
WGYS Admin <westga.youthsports@gmail.com>
2025-12-03 01:40:48 -0500

    Remove code splitting

--- vite.config.ts
+++ vite.config.ts
@@ -8,29 +8,39 @@ export default defineConfig(() => ({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) {
-              return 'vendor';
-            }
-            if (id.includes('@reduxjs/toolkit') || id.includes('react-redux')) {
-              return 'redux';
-            }
-            // if (id.includes('antd') || id.includes('@ant-design')) {
-            //   return 'antd';
-            // }
-            if (id.includes('firebase')) {
-              return 'firebase';
-            }
-            if (id.includes('lexical')) {
-              return 'lexical';
-            }
-            return 'vendor-libs';
-          }
-        },
-      },
-    },
+    // no rollupOptions.manualChunks for now
   },
 }));
+
+// export default defineConfig(() => ({
+//   plugins: [react()],
+//   base: '/wgys/',
+//   build: {
+//     outDir: 'dist',
+//     assetsDir: 'assets',
+//     rollupOptions: {
+//       output: {
+//         manualChunks: (id) => {
+//           if (id.includes('node_modules')) {
+//             if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) {
+//               return 'vendor';
+//             }
+//             if (id.includes('@reduxjs/toolkit') || id.includes('react-redux')) {
+//               return 'redux';
+//             }
+//             // if (id.includes('antd') || id.includes('@ant-design')) {
+//             //   return 'antd';
+//             // }
+//             if (id.includes('firebase')) {
+//               return 'firebase';
+//             }
+//             if (id.includes('lexical')) {
+//               return 'lexical';
+//             }
+//             return 'vendor-libs';
+//           }
+//         },
+//       },
+//     },
+//   },
+// }));
